9f6d35f904125f4dda800acc2932d7f49796e660
tfinley <tfinley@SAV-DEBIAN-L001.adventure.local>
2020-02-28 14:47:46 -0500

    Calculator Switch Case Complete

--- home_project/calculator.cpp
+++ home_project/calculator.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int switch_(char names, int num);
+bool is_valid(int sel);
+bool switch_(int sel, int num);
 int test_selection(int sel);
-int get_number();
-int while_sum(int num);
 int for_sum(int num);
+int while_sum(int num);
 int for_squares(int num);
 int while_squares(int num);
-bool is_valid(int sel);
+void collatz_conjecture(int num);
 
 int main(void) {
     int nu, selection;
@@ -18,27 +18,21 @@ int main(void) {
         <<"1. Run Sum of Numbers as for loop"<<endl
         <<"2. Run Sum of Numbers as while loop"<<endl
         <<"3. Run Sum of Squares as for loop"<<endl
-        <<"4. Run Sum of Squares as while loop"<<endl<<endl
+        <<"4. Run Sum of Squares as while loop"<<endl
+        <<"5. Run Collatz Conjecture"<<endl
+        <<"6. Run All "<<endl<<endl
         <<"Selection: ";
     cin>>selection;
     while (is_valid(selection) == false) {
         cout<<"Please make a valid selection"<<endl;
         cin>>selection;
     }
-    char final_ = test_selection(selection);
-    switch_(final_, nu);
-/*    int nu, total;
-*    nu = get_number();
-*    total = add_while(nu);
-*    cout<<total<<endl;
-*    cin>>nu;
-*    cout<<add_while(nu)<<endl;
-*/
+    switch_(selection, nu);
 }
 
 bool is_valid(int sel) {
     bool y_n;
-    if (sel > 0 && sel <= 4) {
+    if (sel > 0 && sel <= 6) {
         y_n = true;
     } else {
         y_n = false;
@@ -46,46 +40,52 @@ bool is_valid(int sel) {
     return y_n;
 }
 
-int test_selection(int sel) {
-    char final_select;
-    if (sel == 1) {
-        final_select = "for_sum";
-    } else if (sel == 2) {
-        final_select = "while_sum";
-    } else if (sel == 3) {
-        final_select = "for_sum";
-    } else if (sel == 4) {
-        final_select = "while_sum";
-    }
-    return final_select;
-}   
-
-int switch_(char names, int num) {
-    switch(names) {
-        case "for_sum":
+bool switch_(int sel, int num) {
+    switch(sel) {
+        case 1:
             cout<<for_sum(num)<<endl;
             break;
-        case "while_sum":
+        case 2:
             cout<<while_sum(num)<<endl;
             break;
-        case "for_squares":
+        case 3:
+            cout<<for_squares(num)<<endl;
             break;
-        case "while_squares":
+        case 4:
+            cout<<while_squares(num)<<endl;
+            break;
+        case 5:
+            collatz_conjecture(num);
+            break;
+        case 6:
+            cout<<"For Loop Sum of Number n = "<<num<<endl
+                <<for_sum(num)<<endl
+                <<"While Loop Sum of Number n = "<<num<<endl
+                <<while_sum(num)<<endl
+                <<"For Loop Sum of Squares n = "<<num<<endl
+                <<for_squares(num)<<endl
+                <<"While Loop Sum of Squares n = "<<num<<endl
+                <<while_squares(num)<<endl
+                <<"Collatz Conjecture using number n = "<<num<<endl;
+            collatz_conjecture(num);
             break;
         default:
             cout<<"Error, please select a valid entry"<<endl;
             break;
+    }
+    return true;
 }
 
-int get_number() {
-    int n;
-    cout<<"Enter Number: "<<endl;
-    cin>>n;
-    return n;
+int for_sum(int num) {
+    int sum = 0, i = 1;
+    for (i = 1; i <= num; ++i) {
+        sum += i;
+    }
+    return sum;
 }
 
 int while_sum(int num) {
-    int sum = 0, i;
+    int sum = 0, i = 1;
     while (i <= num) {
         sum += i;
         ++i;
@@ -93,11 +93,35 @@ int while_sum(int num) {
     return sum;
 }
 
-int for_sum(int num) {
-    int sum = 0, i;
-    for (i = 1; i <= num; ++i) {
-        sum += i;
+int for_squares(int num) {
+    int i = 0, product = 0;
+    for (i = 0; i <= num; i++) {
+       product += (i * i);
+   }
+   return product;
+} 
+
+int while_squares(int num) {
+    int i = 0, product = 0;
+    while (i < num) {
+        i++;
+        product += (i * i);
     }
-    return sum;
+    return product;
 }
 
+void collatz_conjecture(int num) {
+    int step_count = 0;
+    while (num != 1) {
+        int x = num % 2;
+        if (x == 0) {
+            num = (num / 2);
+            cout<<num<<endl;
+        } else {
+            num = (3 * num) + 1;
+            cout<<num<<endl;
+        }
+        ++step_count;
+    }
+    cout<<"The conjecture took "<<step_count<<" steps"<<endl;
+}
