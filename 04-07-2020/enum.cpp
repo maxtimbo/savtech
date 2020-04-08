827bd7b12bb6fdb0c00127d31e8263b74746e0a6
tfinley <tfinley@WinBeast.pfhome>
2020-04-07 21:58:58 -0400

    final 04-07-2020

--- 04-07-2020/enum.cpp
+++ 04-07-2020/enum.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-enum directions {east, west, north, south};
+enum directions {east=20, west=30, north=40, south=50};
 
 int main() {
     directions dir = east;
