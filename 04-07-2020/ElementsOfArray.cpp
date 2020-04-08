a99e4d1da7d110b6d00928c7ea132043f670c435
tfinley <tfinley@WinBeast.pfhome>
2020-04-07 20:16:16 -0400

    redo

--- 04-07-2020/ElementsOfArray.cpp
+++ 04-07-2020/ElementsOfArray.cpp
@@ -13,35 +13,35 @@ void printArray(const double[], const int);
 double sumArray(const double[], const int);
 
 int main() {
-    double a[size];
+    double moon[size];
     int n;
-    getArray(a, n);
+    getArray(moon, n);
     cout<<"The array has "<<n<<" objects"<<endl;
-    printArray(a, n);
-    cout<<"The sum of all the objects is "<<sumArray(a, n)<<endl;
+    printArray(moon, n);
+    cout<<"The sum of all the objects is "<<sumArray(moon, n)<<endl;
 
     return 0;
 }
 
-void getArray(double a[], int& n) {
+void getArray(double moon[], int& n) {
     cout<<"Enter numbers"<<endl;
     for(n = 0; n < size; n++) {
         cout<<n<<" : ";
-        cin>>a[n];
-        if(a[n]==0) break;
+        cin>>moon[n];
+        if(moon[n]==0) break;
     }
 }
 
-void printArray(const double a[], const int n) {
+void printArray(const double moon[], const int n) {
     for(int i = 0; i < n; i++) {
-        cout<<'\t'<<i<<" : "<<a[i]<<endl;
+        cout<<'\t'<<i<<" : "<<moon[i]<<endl;
     }
 }
 
-double sumArray(const double a[], const int n) {
+double sumArray(const double moon[], const int n) {
     double sum = 0;
     for(int j = 0; j < n; j++) {
-        sum += a[j];
+        sum += moon[j];
     }
     return sum;    
 }
