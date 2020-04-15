80532c1edd0522e860d3d9aea3c1190f24bb779c
tfinley <tfinley@WinBeast.pfhome>
2020-04-14 22:53:52 -0400

    quiz done

--- 04-14-2020/quiz_gradebook.cpp
+++ 04-14-2020/quiz_gradebook.cpp
@@ -4,34 +4,37 @@ using namespace std;
 const int numstu = 3;
 const int numqui = 5;
 typedef int score[numstu][numqui];
-void readscore(score);
-void score_average(score);
+void readscore(score sc);
+void score_average(score sc);
 
 
 int main() {
     score sc;
     cout<<"Enter "<<numqui<<" for each student:"<<endl;
     readscore(sc);
-    cout<<"Quiz average: ";
+    cout<<"Quiz averages: "<<endl;
     score_average(sc);
+    cout<<endl;
     return 0;
 }
 
-void readscore(score s) {
+void readscore(score c) {
     for(int s = 0; s < numstu; s++) {
-        cout << "student "<<s<<" : ";
+        cout << "Student "<<s+1<<":"<<endl;
         for(int q = 0; q < numqui; q++) {
-            cin>>score[s][q];
+            cout<<"            score : ";
+            cin>>c[s][q];
         }
     }
 }
 
-void score_average(score) {
+void score_average(score c) {
     for(int s = 0; s < numstu; s++) {
+        cout<<"               Student "<<s+1<<":"<<endl;
         float sum = 0;
         for(int q = 0; q < numqui; q++) {
-            sum = sum+score[s][q];
-            cout<<"Quiz Average "<<s<<" : "<<sum/numqui;
+            sum = sum+c[s][q];
+            cout<<"                         Average at Quiz "<<q+1<<": "<<sum/numqui<<endl;
         }
     }
 }
