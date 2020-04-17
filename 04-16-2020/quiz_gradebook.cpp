9051e7aedc8ff08697232dc5dafb203a9ca005d8
tfinley <tfinley@WinBeast.pfhome>
2020-04-16 21:44:42 -0400

    Class Average

--- 04-16-2020/quiz_gradebook.cpp
+++ 04-16-2020/quiz_gradebook.cpp
@@ -4,8 +4,9 @@ using namespace std;
 const int numstu = 3;
 const int numqui = 5;
 typedef int score[numstu][numqui];
-void readscore(score sc);
-void score_average(score sc);
+void readscore(score);
+void score_average(score);
+void class_average(score);
 
 
 int main() {
@@ -14,27 +15,41 @@ int main() {
     readscore(sc);
     cout<<"Quiz averages: "<<endl;
     score_average(sc);
+    cout<<"Class average: ";
+    class_average(sc);
     cout<<endl;
     return 0;
 }
 
-void readscore(score c) {
+void readscore(score sc) {
     for(int s = 0; s < numstu; s++) {
         cout << "Student "<<s+1<<":"<<endl;
         for(int q = 0; q < numqui; q++) {
             cout<<"            score : ";
-            cin>>c[s][q];
+            cin>>sc[s][q];
         }
     }
 }
 
-void score_average(score c) {
+void score_average(score sc) {
     for(int s = 0; s < numstu; s++) {
         cout<<"               Student "<<s+1<<":"<<endl;
         float sum = 0;
         for(int q = 0; q < numqui; q++) {
-            sum = sum+c[s][q];
+            sum = sum + sc[s][q];
             cout<<"                         Average at Quiz "<<q+1<<": "<<sum/numqui<<endl;
         }
     }
+}
+
+void class_average(score sc) {
+    float class_av = 0;
+    for(int s = 0; s < numstu; s++) {
+        float sum = 0;
+        for(int q = 0; q < numqui; q++) {
+            sum = sum + sc[s][q];
+        }
+        class_av += sum/numqui;
+    }
+    cout<<class_av/numstu<<endl;
 }
