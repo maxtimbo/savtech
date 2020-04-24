fcd55234058278dbe483c4e4f88aab26d2a032e6
tfinley <tfinley@WinBeast.pfhome>
2020-04-23 20:12:59 -0400

    classes

---
+++ 04232020/classes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class happylane {
+    public:
+        void stayhappy() {
+            cout<<"Blah balh blah"<<endl;
+        }
+};
+
+int main() {
+    happylane happyobject;
+    happyobject.stayhappy();
+    return 0;
+}
